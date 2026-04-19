9eee2ead836dcb5c8f6a1e8ce1b7e90af45cda4b
lixsukagits <wijayayenny879@gmail.com>
2026-04-19 08:57:27 +0700

    tes ubah config biar ga eror non found

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Ganti 'hsk-belajar' dengan nama repo GitHub kamu
 export default defineConfig({
   plugins: [react()],
-  base: '/hsk_1to5_study-simulation/',
+  base: '/HSK_1to5_study-simulation/',
 })
